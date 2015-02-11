a7c5766a2379b649a5cb5bf65a17b329e5396c6c
chansuke <moonset20@gmail.com>
2015-02-11 23:29:53 +0900

    finish pattern matching

--- parser.hs
+++ parser.hs
@@ -87,13 +87,23 @@ instance Show LispVal where show = showVal
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
 
+-- Evaluator
 eval :: LispVal -> ThrowsError LispVal
 eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+
+eval (List [Atom "if", pred, conseq, alt]) = do
+     result <- eval pred
+     case result of
+        Bool False -> eval alt
+        otherwise  -> eval conseq
+
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
+
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitives function args" func)
